1b72a67d88ded11deae8c7ae948a30074928b543
SubduedGaming <76713813+SubduedGaming@users.noreply.github.com>
2021-01-02 00:23:21 +0000

    Organising

---
+++ Other/Learning Pt1.py
@@ -0,0 +1,13 @@
+
+list = ["ass", "cock"]
+
+print(list)
+list.append(input("what would you like to add? "))
+#print(list)
+
+#for lists in list:
+#    if lists == "prick":
+#        print(lists)
+#    elif
+#        print(list)
+

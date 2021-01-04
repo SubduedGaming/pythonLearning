e25c973c3fbd7cace6b42a6402be4c96c26bebe8
SubduedGaming <76713813+SubduedGaming@users.noreply.github.com>
2021-01-04 17:59:30 +0000

    Update main.py

--- PyBot/main.py
+++ PyBot/main.py
@@ -21,15 +21,14 @@ min_val, max_val, min_loc, max_loc = cv.minMaxLoc(search)
 
 if max_val > threshold:
     print("found your button ")
-    
-    #setting top and bottom locations for box
-    top_left = max_loc
-    bottom_right = (top_left[0] + undock_w, top_left[1] + undock_h)
 
     #Getting Dimenstions of target img
     undock_w = undock.shape(1)
     undock_h = undock.shape(0)
     
+    #setting top and bottom locations for box
+    top_left = max_loc
+    bottom_right = (top_left[0] + undock_w, top_left[1] + undock_h)
     
     cv.rectangle(eve_station_img, top_left, bottom_right, color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
 

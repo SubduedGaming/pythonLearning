50bbd9b3a582df43663bd7f16325b063191bcb31
SubduedGaming <76713813+SubduedGaming@users.noreply.github.com>
2021-01-04 17:41:28 +0000

    Add files via upload

--- PyBot/main.py
+++ PyBot/main.py
@@ -2,10 +2,19 @@ import cv2 as cv
 import numpy as np 
 import pyautogui
 
+threshold = 0.85
 
 eve_station_img = cv.imread('eve-screenshot.jpg', cv.IMREAD_UNCHANGED)
 undock = cv.imread('undock-button.jpg', cv.IMREAD_UNCHANGED)
 
 search = cv.matchTemplate(eve_station_img, undock, cv.TM_CCOEFF_NORMED)
 
-cv.imshow('results', search)
+
+#Getting best match location
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(search)
+
+
+if max_val > threshold:
+    print("found your button ")
+else:
+    print("Sorry match not found. ")

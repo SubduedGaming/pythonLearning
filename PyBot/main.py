78bc94b62690f8da1843f75f10034def976b45e8
SubduedGaming <76713813+SubduedGaming@users.noreply.github.com>
2021-01-04 17:44:15 +0000

    Update main.py

--- PyBot/main.py
+++ PyBot/main.py
@@ -16,5 +16,7 @@ min_val, max_val, min_loc, max_loc = cv.minMaxLoc(search)
 
 if max_val > threshold:
     print("found your button ")
+    
+    cv.rectangle(eve_station_img, top_left, bottom_right, color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
 else:
     print("Sorry match not found. ")

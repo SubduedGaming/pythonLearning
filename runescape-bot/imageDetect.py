26380082bb462b14e75ebd2d5f5f1871f5df56b3
SubduedGaming <georgehewitt77@googlemail.com>
2022-01-16 11:00:17 +0000

    more

--- runescape-bot/imageDetect.py
+++ runescape-bot/imageDetect.py
@@ -2,4 +2,9 @@ import cv2 as cv
 import numpy as np 
 
 needle_img = cv.imread('images/playNow.png', cv.IMREAD_UNCHANGED)
-haystack_img = cv.imread('images/haystack.png')
+haystack_img = cv.imread('images/haystack.png', cv.IMREAD_UNCHANGED)
+
+result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+
+cv.imshow('Result', result)
+cv.waitKey()

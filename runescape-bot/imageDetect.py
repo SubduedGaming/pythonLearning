0f13223cc1003d5404c9d8ba878e494f44965495
SubduedGaming <76713813+SubduedGaming@users.noreply.github.com>
2022-02-01 18:35:17 +0000

    1

--- runescape-bot/imageDetect.py
+++ runescape-bot/imageDetect.py
@@ -1,5 +1,8 @@
 import cv2 as cv
-import numpy as np 
+import numpy as np
+
+#import sys
+#sys.path.append('C:\Users/georg/AppData/Local/Programs\Python\Python310\Lib\site-packages')
 
 needle_img = cv.imread('images/playNow.png', cv.IMREAD_UNCHANGED)
 haystack_img = cv.imread('images/haystack.png', cv.IMREAD_UNCHANGED)
